8a61b676cdecb4a845f8d586fb1c838eb7f19aa2
satendra <satykush8092@gmail.com>
2025-08-30 23:41:01 +0530

    chek for codebuild

--- rabbitMQ/queues/email.queue.js
+++ rabbitMQ/queues/email.queue.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-console.log(">>>>>>>>>>>>>>>",process.env.REDIS_HOST, process.env.REDIS_PORT)
+
 const { Queue } = require('bullmq');
 const connection = { host: process.env.REDIS_HOST, port: process.env.port };
 
